8a34987d3e6b877ed08ee90222261eb23199029c
dhreetijain04 <dhreetijain04@gmail.com>
2024-07-14 12:33:17 +0530

    second commit

---
+++ DAY1/flowcharts.java
@@ -0,0 +1,13 @@
+package DAY1;
+import java.util.Scanner;
+class flowcharts{
+    public static void main(String[] args) {
+       //input
+       Scanner sc= new Scanner(System.in);
+       int a= sc.nextInt();
+        //output
+        System.out.println("Value input by user is");
+        System.out.print(a);
+        sc.close();
+    }
+}
