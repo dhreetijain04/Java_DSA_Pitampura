3d1dc7f15a116a72a4474f4ebd02b3f391c3cb3f
dhreetijain04 <dhreetijain04@gmail.com>
2025-08-17 21:54:55 +0530

    Added DemoCode.java with bubble sort for demo PR

---
+++ DemoCode.java
@@ -0,0 +1,24 @@
+public class DemoCode {
+    public static void main(String[] args) {
+        int[] nums = { 5, 2, 8, 1, 3 };
+        bubbleSort(nums);
+
+        System.out.print("Sorted array: ");
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+    }
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
