f5eacf7a1a552aa3f111a36f59196267c2863da3
dhreetijain04 <dhreetijain04@gmail.com>
2025-08-17 21:43:30 +0530

    Demo PR: Added Bubble Sort example

--- DemoPR.java
+++ DemoPR.java
@@ -1,4 +1,21 @@
 public class DemoPR {
-    // Demo PR test change
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 8, 1, 2 };
 
+        // Bubble Sort implementation (easy to review)
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
